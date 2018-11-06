158332a08d6318adb0d5bc47f121edb46725f2d7
杜厚安 <33805265+DuHouAn@users.noreply.github.com>
2018-11-06 15:01:37 +0800

    线程安全策略

---
+++ src/main/java/code_02_threadSafeStrategy/Code_00_Immutable.java
@@ -0,0 +1,22 @@
+package code_02_threadSafeStrategy;
+
+import com.google.common.collect.Maps;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Created by 18351 on 2018/11/6.
+ */
+public class Code_00_Immutable {
+    private static Map<Integer,Integer> map = Maps.newHashMap();
+
+    static {
+        map.put(1,2);
+        map = Collections.unmodifiableMap(map);
+    }
+
+    public static void main(String[] args) {
+        map.put(1,3);
+    }
+}
